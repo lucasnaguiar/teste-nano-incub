fb4f43ae9136904e29f0ec357dfbf81f760fc5ab
Lucas Aguiar <lucasbarbary@gmail.com>
2022-02-22 21:35:15 -0400

    middleware auth nas rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,18 +16,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::redirect('/','/home');
-
-
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::controller(EmployeeController::class)->group(function(){
-    Route::prefix('funcionarios')->group(function () {
-        Route::get('/', 'index')->name('employees.index');
-        Route::get('/cadastrar', 'create')->name('employees.create');
-        Route::post('/cadastrar', 'store')->name('employees.store');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::controller(EmployeeController::class)->group(function() {
+        Route::prefix('funcionarios')->group(function () {
+            Route::get('/', 'index')->name('employees.index');
+            Route::get('/cadastrar', 'create')->name('employees.create');
+            Route::post('/cadastrar', 'store')->name('employees.store');
+        });
     });
 });
-
-
 
 Auth::routes(['register'=> false, 'reset' => false]);
