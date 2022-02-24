3468aa24861284b2af8a2984a6981e035751b756
Lucas Aguiar <lucasbarbary@gmail.com>
2022-02-24 03:33:29 -0500

    admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            ['full_name' => 'Lucas Aguiar', 'username' => 'lucasnaguiar', 'password' => bcrypt('teste@155')]
+        ]);
+    }
+}
